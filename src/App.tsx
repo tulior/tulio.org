9ca5f4c1a0df23aa991bb795ef4e7afb9ce1eca3
Túlio Ribeiro dos Anjos <mail@tulio.org>
2021-09-26 16:54:35 -0400

    Added muiu and topbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,37 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    NavLink,
+    useParams
+} from 'react-router-dom';
+import TopBar from "./shared/TopBar";
+import Ferramentas from "./ferramentas/Ferramentas";
+import Blog from "./blog/Blog";
+import Home from "./home/Home";
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+export default function App() {
+
+    return (
+        <Router>
+            <div>
+                <TopBar />
+
+                <Switch>
+                    <Route path="/ferramentas">
+                        <Ferramentas/>
+                    </Route>
+                    <Route path="/blog">
+                        <Blog/>
+                    </Route>
+                    <Route path="/">
+                        <Home/>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    )
 }
-
-export default App
---
+++ src/home/Home.tsx
@@ -0,0 +1,5 @@
+export default function() {
+    return (
+        <h5>Oi mundo!</h5>
+    )
+}
